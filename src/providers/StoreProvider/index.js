4e377e7acba9910ea93953652fbc9efdb6dd784c
otsybulsky <helg37@gmail.com>
2019-05-31 16:05:09 +0300

    refactor setState StoreProvider

--- src/providers/StoreProvider/index.js
+++ src/providers/StoreProvider/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { StoreContext } from 'shared/context'
 import { saveStateToStorage, loadStateFromStorage } from './utils'
 import uuid from 'uuid/v4'
+import { containerType } from 'shared/types'
 
 const defaultLayout = [
   { i: uuid(), x: 0, y: 0, w: 4, h: 4, moved: false, static: false },
@@ -13,29 +14,30 @@ const StoreProvider = ({ windowSize, children }) => {
     fullscreen: false,
     containerId: null,
     layout: [],
-    backupLayout: []
+    backupLayout: [],
+    containerStore: {}
   })
 
   const [isStart, setIsStart] = useState(true)
   const cols = 12
 
   const onFullscreenContainer = containerId => {
-    setState({
+    setState(state => ({
       ...state,
       containerId,
       fullscreen: true,
-      backupLayout: state.layout.map(item => ({ i: item.i, ...item })),
+      backupLayout: state.layout,
       layout: [] //clear layout and useEffect state.fulscreen
-    })
+    }))
   }
 
   const onRestoreLayout = () => {
-    setState({
+    setState(state => ({
       ...state,
       fullscreen: false,
       containerId: null,
       layout: [] //clear layout and useEffect state.fulscreen
-    })
+    }))
   }
 
   useEffect(() => {
@@ -49,73 +51,94 @@ const StoreProvider = ({ windowSize, children }) => {
         static: true
       }
 
-      setState({
+      setState(state => ({
         ...state,
         layout: [container]
-      })
+      }))
     }
 
     if (!state.fullscreen && !state.containerId) {
-      setState({
+      setState(state => ({
         ...state,
         layout: state.backupLayout
-      })
+      }))
     }
   }, [state.fullscreen])
 
   const onLayoutChange = newLayout => {
-    setState({
+    setState(state => ({
       ...state,
       layout: newLayout
-    })
+    }))
   }
 
   const removeItemFromLayout = id => {
-    setState({
+    setState(state => ({
       ...state,
       layout: state.layout.filter(item => item.i !== id)
-    })
+    }))
+  }
+
+  const initContainerStore = containerId => {
+    return {
+      containerId,
+      data: {},
+      config: {
+        type: containerType.Chart,
+        symbol: 'SPY'
+      }
+    }
   }
 
   const addItemToLayout = () => {
-    setState({
+    const id = uuid()
+    setState(state => ({
       ...state,
+      containerStore: {
+        ...state.containerStore,
+        [id]: initContainerStore(id)
+      },
       layout: [
         ...state.layout,
         {
-          i: uuid(),
+          i: id,
           x: 0,
           y: Infinity, // puts it at the bottom
           w: 4,
           h: 4
         }
       ]
-    })
+    }))
   }
 
   useEffect(() => {
     const restored = loadStateFromStorage()
     if (restored && restored.config) {
-      setState({ ...state, ...restored.config.state })
+      setState(state => ({ ...state, ...restored.config.state }))
     } else {
-      setState({ ...state, layout: defaultLayout })
+      let containerStore = {}
+      defaultLayout.forEach(item => {
+        containerStore = {
+          ...containerStore,
+          [item.i]: initContainerStore(item.i)
+        }
+      })
+
+      setState(state => ({ ...state, containerStore, layout: defaultLayout }))
     }
     setIsStart(false)
   }, []) //componentDidMount
 
   useEffect(() => {
     if (!isStart) {
-      //reconfigure for autoupdate after restore
-      const _lay = state.layout.map(item => ({ i: item.i, ...item }))
-
-      saveStateToStorage({ config: { state: { ...state, layout: _lay } } })
+      saveStateToStorage({ config: { state } })
     }
   }, [state])
 
   return (
     <StoreContext.Provider
       value={{
-        layout: state.layout,
+        layout: [...state.layout],
         onLayoutChange,
         addItemToLayout,
         removeItemFromLayout,
---
+++ src/shared/types.js
@@ -0,0 +1,4 @@
+export const containerType = {
+  Chart: 'chart',
+  WatchList: 'watch list'
+}
